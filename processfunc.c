bc2c8299f3d38d991f944cf40fd4ec4f08b2b61b
DucEMB <137813610+DucEMB@users.noreply.github.com>
2023-10-25 10:25:16 +0700

    add function

--- processfunc.c
+++ processfunc.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "processfunc.h"
 
 void write_to_file(uint8_t animal_type[])
@@ -5,6 +6,13 @@ void write_to_file(uint8_t animal_type[])
     FILE * file = fopen("output.txt","w+");
     fprintf(file,"%s\n",animal_type);
     fclose(file);
+}
+
+int get_random()
+{
+    int res = 0;
+    res = MIN_RAND + (int)(rand()*(MAX_RAND - MIN_RAND +1.0)/(1.0 + RAND_MAX));
+    return res;
 }
 
 /********* END OF FILE *********/
