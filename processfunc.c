8405e5845602bac4dbd9ec0730438d80623a32ee
DucEMB <137813610+DucEMB@users.noreply.github.com>
2023-10-25 10:28:04 +0700

    add function

--- processfunc.c
+++ processfunc.c
@@ -11,7 +11,7 @@ void write_to_file(uint8_t animal_type[])
 int get_random()
 {
     int res = 0;
-    res = MIN_RAND + (int)(rand()*(MAX_RAND - MIN_RAND +1.0)/(1.0 + RAND_MAX));
+    res = MIN_RAND + (int)(rand()*(MAX_RAND - MIN_RAND + 1.0)/(1.0 + RAND_MAX));
     return res;
 }
 
