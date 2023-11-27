b4d9c926e9514404e3a4acd9ec34963940ed361b
DucEMB <137813610+DucEMB@users.noreply.github.com>
2023-11-27 09:12:26 +0700

    done

---
+++ palindrome_swapping-hackerearth.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdbool.h>
+#include<malloc.h>
+#include<string.h>
+
+int main()
+{
+    char* T = (char*)malloc(200000 * sizeof(char));
+    scanf("\n%[^\n]s", T);
+    char* S = (char*)malloc(200000 * sizeof(char));
+    scanf("\n%[^\n]s", S);
+    int i;
+    int cnt[256] = {0};
+    for (i = 0;i < strlen(T);i++)
+    {
+        cnt[T[i]]++;
+        cnt[S[i]]++;
+    }
+    for (i = 0;i < 256;i++)
+    {
+        if (cnt[i] > 0 && cnt[i] % 2 != 0)
+        {
+            printf("NO\n");
+            return 0;
+        }
+    }
+    printf("YES\n");
+    return 0;
+}
